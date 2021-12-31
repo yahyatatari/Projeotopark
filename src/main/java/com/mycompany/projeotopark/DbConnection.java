13aa572a81fe5e1d71909fbfad9214cf30c85def
ASUS <ASUS@LAPTOP-2QGFA5B8>
2021-12-31 00:18:57 -0800

    first commit

---
+++ src/main/java/com/mycompany/projeotopark/DbConnection.java
@@ -0,0 +1,20 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+
+    private String userName = "root"; 
+    private String password = "12345";
+    private String dbUrl = "jdbc:mysql://localhost:3306/aractakip";
+    
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(dbUrl, userName, password);
+    }
+    
+    public void showErrorMessage(SQLException exception) {
+        System.out.println("Error: " + exception.getMessage());
+        System.out.println("Error Code:" + exception.getErrorCode());
+}
+}
